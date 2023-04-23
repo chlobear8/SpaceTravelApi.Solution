33766d93f58033cfefe5e0cd933959ea5a4e7b68
Chloe ONeil <chloesporri@yahoo.com>
2023-04-23 15:19:02 -0700

    delete Identity

--- SpaceTravel/Program.cs
+++ SpaceTravel/Program.cs
@@ -1,10 +1,7 @@
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using SpaceTravel.Data;
 using SpaceTravel.Models;
 using RestSharp;
 //using SpaceTravel.Keys;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,9 +10,6 @@ builder.Services.AddControllersWithViews();
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-// builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//     options.UseSqlite(connectionString));
-// builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDbContext<SpaceTravelContext>(
                   dbContextOptions => dbContextOptions
@@ -25,23 +19,19 @@ builder.Services.AddDbContext<SpaceTravelContext>(
                   )
                 );
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
-{
+app.Environment.IsDevelopment();
+
+
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -54,6 +44,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
 
 app.Run();
