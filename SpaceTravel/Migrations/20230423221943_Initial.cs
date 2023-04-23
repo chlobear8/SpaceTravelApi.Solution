64f3411e7c843e751709466598f2cc67e2a84f1a
Chloe ONeil <chloesporri@yahoo.com>
2023-04-23 16:46:08 -0700

    final day commit and push

---
+++ SpaceTravel/Migrations/20230423221943_Initial.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpaceTravel.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Destination",
+                columns: table => new
+                {
+                    DestinationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Price = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Url = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Destination", x => x.DestinationId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Destination",
+                columns: new[] { "DestinationId", "Name", "Price", "Url" },
+                values: new object[,]
+                {
+                    { 1, "Carina Nebula", "$1,295", "dfhdfj" },
+                    { 2, "Helix Nebula", "$1,495", "https://images.nasa.gov/details/PIA09178" },
+                    { 3, "Mars", "$1,095", "dhhhf" },
+                    { 4, "Neptune ", "$2,295", "https://images.nasa.gov/details/PIA01492" },
+                    { 5, "Aurora Australis", "$1,895", "https://images.nasa.gov/details/iss029e008433" },
+                    { 6, "Carina Nebula2", "$1,495", "dsgsdg" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Destination");
+        }
+    }
+}
---
+++ SpaceTravel/Migrations/20230423230749_UpdatedSeedData.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpaceTravel.Migrations
+{
+    public partial class UpdatedSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Destination",
+                keyColumn: "DestinationId",
+                keyValue: 1,
+                column: "Url",
+                value: "https://images-assets.nasa.gov/image/carina_nebula/carina_nebula~medium.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Destination",
+                keyColumn: "DestinationId",
+                keyValue: 2,
+                column: "Url",
+                value: "https://images-assets.nasa.gov/image/PIA09178/PIA09178~medium.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Destination",
+                keyColumn: "DestinationId",
+                keyValue: 3,
+                column: "Url",
+                value: "https://images-assets.nasa.gov/image/PIA21041/PIA21041~small.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Destination",
+                keyColumn: "DestinationId",
+                keyValue: 4,
+                column: "Url",
+                value: "https://images-assets.nasa.gov/image/PIA01492/PIA01492~medium.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Destination",
+                keyColumn: "DestinationId",
+                keyValue: 5,
+                columns: new[] { "Name", "Url" },
+                values: new object[] { "Blazar Black Hole", "https://images-assets.nasa.gov/image/PIA20912/PIA20912~small.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Destination",
+                keyColumn: "DestinationId",
+                keyValue: 6,
+                columns: new[] { "Name", "Url" },
+                values: new object[] { "Crab Nebula", "https://images-assets.nasa.gov/image/PIA21474/PIA21474~small.jpg" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Destination",
+                keyColumn: "DestinationId",
+                keyValue: 1,
+                column: "Url",
+                value: "dfhdfj");
+
+            migrationBuilder.UpdateData(
+                table: "Destination",
+                keyColumn: "DestinationId",
+                keyValue: 2,
+                column: "Url",
+                value: "https://images.nasa.gov/details/PIA09178");
+
+            migrationBuilder.UpdateData(
+                table: "Destination",
+                keyColumn: "DestinationId",
+                keyValue: 3,
+                column: "Url",
+                value: "dhhhf");
+
+            migrationBuilder.UpdateData(
+                table: "Destination",
+                keyColumn: "DestinationId",
+                keyValue: 4,
+                column: "Url",
+                value: "https://images.nasa.gov/details/PIA01492");
+
+            migrationBuilder.UpdateData(
+                table: "Destination",
+                keyColumn: "DestinationId",
+                keyValue: 5,
+                columns: new[] { "Name", "Url" },
+                values: new object[] { "Aurora Australis", "https://images.nasa.gov/details/iss029e008433" });
+
+            migrationBuilder.UpdateData(
+                table: "Destination",
+                keyColumn: "DestinationId",
+                keyValue: 6,
+                columns: new[] { "Name", "Url" },
+                values: new object[] { "Carina Nebula2", "dsgsdg" });
+        }
+    }
+}
