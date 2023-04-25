e451b89e7ff76887fc07861782acd8fc98cd0804
Chloe ONeil <chloesporri@yahoo.com>
2023-04-24 19:59:13 -0700

    add ImageModel and routes in HomeController

--- SpaceTravel/Controllers/HomeController.cs
+++ SpaceTravel/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using SpaceTravel.Models;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace SpaceTravel.Controllers;
 
@@ -15,7 +18,52 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        List<ImageModel> images = GetImages();
+        return View(images);
+    }
+
+    [HttpPost]
+    public ActionResult Index(int imageId)
+    {
+        List<ImageModel> images = GetImages();
+        ImageModel image = images.Find(p => p.Id == imageId);
+        if (image != null)
+        {
+            image.IsSelected = true;
+            ViewBag.Base64String = "data:image/png;base64" + Convert.ToBase64String(image.Data, 0, image.Data.Length);
+        }
+        return View(images);
+    }
+
+    private List<ImageModel> GetImages()
+    {
+        string query = "SELECT * FROM tblFiles";
+        List<ImageModel> images = new List<ImageModel>();
+        string constr = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
+        using (SqlConnection con = new SqlConnection(constr))
+        {
+            using (SqlCommand cmd = new SqlCommand(query))
+            {
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = con;
+                con.Open();
+                using (SqlDataReader sdr = cmd.ExecuteReader())
+                {
+                    while (srd.Read())
+                    {
+                        images.Add(new ImageModel
+                        {
+                            Id = Convert.ToInt32(sdr["Id"]),
+                            Name = sdr["Name"].ToString(),
+                            ContentType = sdr["ContentType"].ToString(),
+                            Data = (byte[])sdr["Data"]
+                        });
+                    }
+                }
+                con.Close();
+            }
+            return images;
+        }
     }
 
     public IActionResult Privacy()
