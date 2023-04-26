be3362faff96ac98717bf07412e430515ba1c625
Chloe ONeil <chloesporri@yahoo.com>
2023-04-25 20:14:26 -0700

    WIP

--- SpaceTravel/Controllers/HomeController.cs
+++ SpaceTravel/Controllers/HomeController.cs
@@ -21,52 +21,7 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        List<ImageModel> images = GetImages();
-        return View(images);
-    }
-
-    [HttpPost]
-    public IActionResult Index(int imageId)
-    {
-        List<ImageModel> images = GetImages();
-        ImageModel image = images.FirstOrDefault(p => p.Id == imageId);
-        if (image != null)
-        {
-            image.IsSelected = true;
-            ViewBag.Base64String = "data:image/png;base64" + Convert.ToBase64String(image.Data, 0, image.Data.Length);
-        }
-        return View(images);
-    }
-
-    private List<ImageModel> GetImages()
-    {
-        string query = "SELECT * FROM tblFiles";
-        List<ImageModel> images = new List<ImageModel>();
-        string constr = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
-        using (SqlConnection con = new SqlConnection(constr))
-        {
-            using (SqlCommand cmd = new SqlCommand(query))
-            {
-                cmd.CommandType = CommandType.Text;
-                cmd.Connection = con;
-                con.Open();
-                using (SqlDataReader sdr = cmd.ExecuteReader())
-                {
-                    while (srd.Read())
-                    {
-                        images.Add(new ImageModel
-                        {
-                            Id = Convert.ToInt32(sdr["Id"]),
-                            Name = sdr["Name"].ToString(),
-                            ContentType = sdr["ContentType"].ToString(),
-                            Data = (byte[])sdr["Data"]
-                        });
-                    }
-                }
-                con.Close();
-            }
-            return images;
-        }
+        return View();
     }
 
     public IActionResult Privacy()
