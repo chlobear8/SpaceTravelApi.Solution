3f9b8522da6ed26d4494c5f4957700505ec21d98
Chloe ONeil <chloesporri@yahoo.com>
2023-05-02 18:52:45 -0700

    WIP css for api

--- SpaceTravel/Models/ApiHelper.cs
+++ SpaceTravel/Models/ApiHelper.cs
@@ -8,7 +8,8 @@ namespace SpaceTravel.Models
     public static async Task<string> ApiCall(string apiKey)
     {
       RestClient client = new RestClient("https://api.nasa.gov");
-      RestRequest request = new RestRequest($"planetary/apod?api_key=NASA");
+      // RestRequest request = new RestRequest($"planetary/apod?api_key={apiKey}", Method.Get);
+      RestRequest request = new RestRequest($"planetary/apod?api_key=OMWEfEkRmo4Yp4Sh7z6Ic6nezdbwPrfS7VE89NqC");
       RestResponse response = await client.ExecuteAsync(request);
       return response.Content;
     }
